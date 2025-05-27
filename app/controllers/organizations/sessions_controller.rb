3032f2cfdc10306a90878517824a817954ffad99
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-05-27 09:56:27 -0700

    add lvh to redirect

--- app/controllers/organizations/sessions_controller.rb
+++ app/controllers/organizations/sessions_controller.rb
@@ -3,7 +3,7 @@ class Organizations::SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate!(auth_options)
 
-    redirect_to root_url(subdomain: resource.subdomain, host: "localhost", protocol: "http"), allow_other_host: true
+    redirect_to root_url(subdomain: resource.subdomain, host: "lvh.me", protocol: "http"), allow_other_host: true
 
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
