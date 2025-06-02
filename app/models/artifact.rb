0177ab83a69434af642903fdb253a61f402f7cdf
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-06-02 15:51:16 -0700

    Create Artifacts validations

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,3 +1,11 @@
 class Artifact < ApplicationRecord
   belongs_to :project
+  attr_accessor :upload
+  MAX_FILE_SIZE = 10.megabytes
+
+  def uploaded_file_size
+    if upload
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :organization
 
+  has_many :artifacts
+
   validates :title, presence: true
   validates :details, presence: true
   validates :expected_completion_date, presence: true
