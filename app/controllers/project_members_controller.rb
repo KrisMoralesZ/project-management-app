566b87a9c3aa850740115140c9fd76c226013cfc
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-06-11 09:14:14 -0700

    search members

--- app/controllers/project_members_controller.rb
+++ app/controllers/project_members_controller.rb
@@ -1,2 +1,17 @@
 class ProjectMembersController < ApplicationController
+  before_action :set_project
+
+  def search
+    query = params[:query]
+    @results = @project.organization.users
+                       .where.not(id: @project.users.pluck(:id))
+                       .where("email ILIKE :q OR name ILIKE :q", q: "%#{query}%")
+    render :search
+  end
+
+
+  private
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
 end
