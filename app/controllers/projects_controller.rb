5908201976024c2c2eeca924ee77a4321a607c53
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-06-12 18:41:22 -0700

    add projects by tenant

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects or /projects.json
   def index
-    @projects = current_organization.projects.all
+    @projects = ActsAsTenant.current_tenant.projects
   end
 
   # GET /projects/1 or /projects/1.json
