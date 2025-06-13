54fcecd31702153146a2fbc377bc5a783af51055
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-06-12 17:48:07 -0700

    create accordion to project_members actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1 or /projects/1.json
   def show
+    @project_members = @project.project_members
   end
 
   # GET /projects/new
